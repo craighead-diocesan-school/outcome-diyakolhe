a71eff5971ae27308a3c849ada963b4f625b96c5
diyakolhe <kolhedi61@craighead.school.nz>
2024-06-24 09:42:55 +1200

    Menu Items
    Made an array of objects which is the menu items and displayed them on the page in a grid with their name and price and with a add button next to it. The button currently does not do anything but it will add the item to the cart. The actual rest of the menu items need to be added to the array.

--- src/routes/Menu/+page.svelte
+++ src/routes/Menu/+page.svelte
@@ -4,16 +4,45 @@
   import Footer from "$lib/Footer.svelte"
   import MenuItem from "$lib/MenuItem.svelte"
 
+  let items = [
+    { name: "Chicken Lollipop", price: 15, image: "/chickenlollipop.png" },
+    { name: "Butter Chicken", price: 19, image: "/chickenlollipop.png" },
+    { name: "Chilli Chicken", price: 19, image: "/chickenlollipop.png" },
+    { name: "Butter Chicken", price: 19, image: "/chickenlollipop.png" },
+    { name: "Chilli Chicken", price: 19, image: "/chickenlollipop.png" },
+  ]
 </script>
 
-<Header />
-<Nav />
+<body>
+  <Header />
+  <Nav />
+  <div class="container">
+    {#each items as item}
+      <div class="item">
+        <img src={item.image} alt="food" />
+        <p>{item.name} ${item.price} <button>Add</button></p>
+      </div>
+    {/each}
+  </div>
 
-<div>
-  <MenuItem />
-  <MenuItem />
-  <MenuItem />
-  <MenuItem />
-</div>
+  <Footer />
+</body>
 
-<Footer />
+<style>
+  body {
+    background-color: black;
+  }
+  img {
+    height: 5cm;
+    width: 5cm;
+  }
+
+  p {
+    color: white;
+  }
+
+  .container {
+    display: grid;
+    grid-template-columns: auto auto auto auto auto;
+  }
+</style>
